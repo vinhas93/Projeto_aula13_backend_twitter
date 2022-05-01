2ddfc7dc2b7ff2028109ec09351eef89917e2851
Daniel Vinhas <vinhas.daniel@gmail.com>
2022-05-01 16:48:42 -0300

    add auth middleware

--- src/users/users.service.js
+++ src/users/users.service.js
@@ -8,9 +8,12 @@ const createUserService = (body) => User.create(body);
 
 const findAllUserService = () => User.find()
 
+const findByIdUserService = (idUser) => User.findById(idUser)
+
 module.exports = {
     findByEmeilUserService,
     createUserService,
     findByUsernameUserService,
-    findAllUserService
+    findAllUserService,
+    findByIdUserService,
 }
