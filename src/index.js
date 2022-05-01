07ed32cc197c80e6d150de178c557f437b8d1a5b
Daniel Vinhas <vinhas.daniel@gmail.com>
2022-05-01 10:52:54 -0300

    Mongodb connect

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const connectDatabase = require('./database/database');
 
 const port = process.env.PORT || 3001;
 const app = express();
 
+connectDatabase()
+
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-	res.send({ message: `Hello, World!` });
-});
 
 app.listen(port, () => {
 	console.log(`servidor rodando na porta ${port}`);
