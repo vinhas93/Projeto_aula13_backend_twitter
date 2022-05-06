8dff726ec76367eb2584ed22a60e24263e44a0d9
Daniel Vinhas <vinhas.daniel@gmail.com>
2022-05-06 20:13:32 -0300

    Swagger

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const connectDatabase = require('./database/database');
 const userRoute = require('./users/users.route');
 const authRoute = require('./auth/auth.route');
 const tweetsRoute = require('./tweets/tweets.route');
+const swaggerRoute = require("./swagger/swagger.route");
 
 const port = process.env.PORT || 3001;
 const app = express();
@@ -18,6 +19,7 @@ app.use(express.json());
 app.use('/users', userRoute);
 app.use('/auth', authRoute);
 app.use('/tweets', tweetsRoute);
+app.use("/api-docs", swaggerRoute);
 
 app.listen(port, () => {
   console.log(`servidor rodando na porta ${port}`);
