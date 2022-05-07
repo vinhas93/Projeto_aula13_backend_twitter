20a6e6a0a6c1261ce2d6b0eb39f20b9806a8da83
Daniel Vinhas <vinhas.daniel@gmail.com>
2022-05-06 21:01:49 -0300

    remove consoles

--- src/tweets/tweets.controller.js
+++ src/tweets/tweets.controller.js
@@ -2,19 +2,18 @@ const tweetService = require('./tweets.service');
 
 const createTweetController = async (req, res) => {
   try {
-    console.log('olá');
-    const { message } = req.body;
+        const { message } = req.body;
 
     if (!message) {
       res.status(400).send({
         message: 'Envie todos os dados necessários para a criação do Tweet.',
       });
     }
-    console.log('olá 2' + req.userId);
+   
 
     const { id } = await tweetService.createTweetService(message, req.userId);
 
-    console.log('olá 3');
+    
     return res.status(201).send({
       message: 'Tweet Criado com sucesso',
       tweet: { id, message },
